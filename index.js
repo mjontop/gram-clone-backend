1e91283a60dc0ace8285549d427ca9c7ed7f0e8d
Manikant Jha <mjontop0602@gmail.com>
2021-10-03 01:41:25 +0530

    Increased Payload data limt to 5mb

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
   });
 
 //Middlewares
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 app.use("/api/user/", authRoutes);
 app.use("/api/post/", postRoutes);
